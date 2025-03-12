c8c2b57ee766c7228ef0f6bf12cd5edf2ae65c49
Kolad2 <koladik@gmail.com>
2025-03-12 05:42:55 +0300

    update 6

--- hero/move.gd
+++ hero/move.gd
@@ -1,44 +1,56 @@
 extends Node2D
 
+enum Direction { NULL, LEFT, RIGHT, UP, DOWN }
+#enum Animation { STAND, WALK, RUN }
+
+# Сохраняем ссылку на AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D
+@export var direction: Direction = Direction.DOWN
 
 func _process(delta):
-	var animated_sprite = get_node("AnimatedSprite2D")
-	var dx = 0
-	var dy = 0
-	
 	var norm_v = get_norm_v()
-	var direction = get_direction(norm_v)
-	if direction == "stand":
+	var move_direction = get_direction(norm_v)
+	if move_direction == Direction.NULL:
+		set_stand_animation(direction)
 		return
-		
+	else:
+		direction = move_direction
 	position["x"] = position["x"] + 1*norm_v["x"]
 	position["y"] = position["y"] + 1*norm_v["y"]
-	
-	if abs(norm_v["x"]) > abs(norm_v["y"]):
-		if norm_v["x"] > 0:
-			animated_sprite.animation = "stand_right"
-		else:
-			animated_sprite.animation = "stand_left"
-	else:
-		if norm_v["y"] > 0:
-			animated_sprite.animation = "stand_down"
-		else:
-			animated_sprite.animation = "stand_up"
+	set_walk_animation(direction)
 		
+
+func set_walk_animation(dir):
+	var stand_animation = {
+		Direction.RIGHT: "walk_right",
+		Direction.LEFT: "walk_left",
+		Direction.UP: "walk_up",
+		Direction.DOWN: "walk_down",
+		}
+	animated_sprite.animation = stand_animation[dir]
+
+func set_stand_animation(dir):
+	var stand_animation = {
+		Direction.RIGHT: "stand_right",
+		Direction.LEFT: "stand_left",
+		Direction.UP: "stand_up",
+		Direction.DOWN: "stand_down",
+		}
+	animated_sprite.animation = stand_animation[dir]
 
 func get_direction(norm_v):
 	if norm_v["x"] == 0 and norm_v["y"] == 0:
-		return "stand"
+		return Direction.NULL
 	if abs(norm_v["x"]) > abs(norm_v["y"]):
 		if norm_v["x"] > 0:
-			return "right"
+			return Direction.RIGHT
 		else:
-			return "left"
+			return Direction.LEFT
 	else:
 		if norm_v["y"] > 0:
-			return "down"
+			return Direction.DOWN
 		else:
-			return "up"
+			return Direction.UP
 	
 	
 
@@ -47,16 +59,12 @@ func get_norm_v():
 	var norm_dy = 0
 	if Input.is_action_pressed("move_up"):
 		norm_dy = -Input.get_action_strength("move_up")
-	
-	if Input.is_action_pressed("move_down"):
+	elif Input.is_action_pressed("move_down"):
 		norm_dy = Input.get_action_strength("move_down")
-	
 	if Input.is_action_pressed("move_right"):
-		norm_dx = Input.get_action_strength("move_right")
-	
-	if Input.is_action_pressed("move_left"):
+		norm_dx = Input.get_action_strength("move_right")	
+	elif Input.is_action_pressed("move_left"):
 		norm_dx = -Input.get_action_strength("move_left")
-	
 	return {"x": norm_dx, "y": norm_dy}
 	
 	
---
+++ readme.md
@@ -0,0 +1,5 @@
+Сохранить учетные данные навсегда (не рекомендуется для токенов)
+git config --global credential.helper store
+Добавление в отслеживание
+git add .
+git commit -m "massage"
