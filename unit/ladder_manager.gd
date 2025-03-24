edee6c6d94479d029af2270729f5ce160a813fb5
koladik <koladik@gmail.com>
2025-03-24 15:39:25 +0300

    add StairsManager

--- unit/ladder_manager.gd
+++ unit/ladder_manager.gd
@@ -1,1 +1,10 @@
-extends Node
+extends Node2D
+
+class_name StairsManager
+
+@onready var area_stairs_lower: Area2D = Area2D.new()
+@onready var area_stairs_upper: Area2D = Area2D.new()
+
+func _ready() -> void:
+	pass
+	
